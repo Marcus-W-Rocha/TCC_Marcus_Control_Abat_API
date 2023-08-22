79354101e52c8a7a0c79b97ba04b3d26eebdfeff
Marcus-W-Rocha <ma.wel@live.com>
2023-08-22 08:27:45 -0300

    Adição user

--- Database/droptables.sql
+++ Database/droptables.sql
@@ -1,7 +1,12 @@
 -- SQLite
-DROP TABLE clientes;
-DROP TABLE pedidos;
 
+idCliente INTEGER PRIMARY KEY,
+    nomeEmpresa TEXT NOT NULL,
+    nomeRepresentante TEXT NOT NULL,
+    numRepresentante TEXT NOT NULL,
+    username TEXT NOT NULL,
+    senha TEXT NOT NULL
+e10adc3949ba59abbe56e057f20f883e
 /* DROP TABLE clientes;
 DROP TABLE clientes;
 DROP TABLE clientes;
